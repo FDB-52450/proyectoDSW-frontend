8648d4d1ed3205bc8f110b0a0c286d451441ad2c
denborum <denborum@water.com>
2025-09-12 02:56:48 -0300

    v4.3.0: - Implemented custom notification system with 4 pre-built notifications components: create, update, delete and error.

---
+++ src/notifications/customNotifications.tsx
@@ -0,0 +1,47 @@
+import { notifications } from '@mantine/notifications';
+
+import { IconAlertTriangle, IconCheck, IconPlus, IconTrash } from '@tabler/icons-react';
+
+export function pushCreateNotification(tipo: string) {
+    notifications.show({
+        title: 'Entidad creada con exito',
+        message: `La ${tipo.slice(0, -1)} ha sido creada con exito.`,
+        color: 'blue',
+        position: 'bottom-center',
+        icon: <IconPlus size={20} />,
+        autoClose: 5000
+    })
+}
+
+export function pushUpdateNotification(tipo: string) {
+    notifications.show({
+        title: 'Entidad actualizada con exito',
+        message: `La ${tipo.slice(0, -1)} ha sido actualizada con exito.`,
+        color: 'green',
+        position: 'bottom-center',
+        icon: <IconCheck size={20} />,
+        autoClose: 5000
+    })
+}
+
+export function pushDeleteNotification(tipo: string) {
+    notifications.show({
+        title: 'Entidad borrada con exito',
+        message: `La ${tipo.slice(0, -1)} ha sido borrada con exito.`,
+        color: 'red',
+        position: 'bottom-center',
+        icon: <IconTrash size={20} />,
+        autoClose: 5000
+    })
+}
+
+export function pushErrorNotification(codigo: number, message: string) {
+    notifications.show({
+        title: 'Ha ocurrido un error!',
+        message: `[ERROR ${codigo}]: ${message}`,
+        color: 'red',
+        position: 'bottom-center',
+        icon: <IconAlertTriangle size={20} />,
+        autoClose: false
+    })
+}
