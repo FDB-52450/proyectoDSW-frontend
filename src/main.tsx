2641b30b8bba590c6a7e2c4fb3de990ad1a67e67
denborum <denborum@water.com>
2025-09-15 19:01:41 -0300

    v4.5.1: - Added mantine/dates and dayjs libraries for date input support. - Added embla-carousel-autoplay library for autoplay carousel support.

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,26 @@
 import '@mantine/core/styles.css'
+
 import '@mantine/notifications/styles.css'
 import '@mantine/carousel/styles.css'
 import '@mantine/dropzone/styles.css'
+import '@mantine/dates/styles.css'
+
+import 'dayjs/locale/es'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import { MantineProvider } from '@mantine/core'
+import { DatesProvider } from '@mantine/dates'
 
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <MantineProvider>
-            <App/>
+            <DatesProvider settings={{ locale: 'es' }}>
+                <App/>
+            </DatesProvider>
         </MantineProvider>
     </StrictMode>,
 )
